a0b0b0526e4700e72302adc4f7bedd5b689d242a
Sebastian Dassé <sebdas@web.de>
2014-10-19 20:06:12 +0200

    Getting there.

--- UE01-1/UE01-1/fix_point.cpp
+++ UE01-1/UE01-1/fix_point.cpp
@@ -1,7 +1,8 @@
 #include <cstdint>
 #include "fix_point.h"
 
-static const int Q = 16;
+static const int Q = 16; // number of bits of the fractional part
+static const int R = 1 << Q; // number of bits of the fractional part
 
 fix_point to_fix_point(float value) {
     return { std::int32_t(value * (1 << Q)) };
@@ -13,12 +14,13 @@ float to_float(fix_point value) {
 
 // TODO -> only integer operations allowed
 float floor(fix_point value) {
-    float sign = 1;
-    if (value.data & 0x80000000) {
-        sign = -1;
-    }
+//    float sign = 1;
+//    if (value.data & 0x80000000) {
+//        sign = -1;
+//    }
 
-    return (float(value.data & 0x7fff0000) / (1 << Q));
+//    return (float(value.data & 0x7fff0000) / (1 << Q));
+    return (float(value.data >> Q));
 };
 
 // TODO -> not working correctly
@@ -35,30 +37,37 @@ bool less_than(fix_point lhs, fix_point rhs) {
     return lhs.data < rhs.data;
 };
 
+//---- prevent over-/underflow
+//std::int32_t to_int32_t_with_saturation(std::int64_t n) {
+//    if (n > std::int64_t(0x7fffffff)) {
+//        n = 0x7fffffff;
+//    } else if (n < std::int64_t(-0x7fffffff)) {
+//        n = -0x7fffffff;
+//    }
+//    return std::int32_t(n);
+//}
+
 fix_point add(fix_point lhs, fix_point rhs) {
     std::int64_t temp = lhs.data + rhs.data;
-    if (temp > std::int64_t(0x7fffffff)) {
-        temp = 0x7fffffff;
-    } else if (temp < std::int64_t(-0x7fffffff)) {
-        temp = -0x7fffffff;
-    }
     return { std::int32_t(temp) };
+//    return { to_int32_t_with_saturation(temp) };
 };
 
-// TODO -> prevent over-/underflow
 fix_point sub(fix_point lhs, fix_point rhs) {
     std::int64_t temp = lhs.data - rhs.data;
     return { std::int32_t(temp) };
+//    return { to_int32_t_with_saturation(temp) };
 };
 
-// TODO -> prevent over-/underflow
 fix_point mul(fix_point lhs, fix_point rhs) {
-    std::int64_t temp = lhs.data * rhs.data;
-    return { std::int32_t(temp) };
+    std::int64_t temp = std::int64_t(lhs.data) * std::int64_t(rhs.data);
+//    temp += (1 << (Q-1)); // mid values are rounded up
+    return { std::int32_t(temp >> 16) };
 };
 
-// TODO -> prevent over-/underflow and division by zero
 fix_point div(fix_point lhs, fix_point rhs) {
-    std::int64_t temp = lhs.data / rhs.data;
-    return { std::int32_t(temp) };
+    std::int64_t temp = std::int64_t(lhs.data) << Q;
+//    temp += rhs.data / 2; // mid values are rounded up
+    std::int64_t result = temp / std::int64_t(rhs.data);
+    return { std::int32_t(result) };
 };
--- UE01-1/UE01-1/main.cpp
+++ UE01-1/UE01-1/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+//    cout << (7 * (1 << 16) * 3 * (1 << 16)) << endl;
+//    cout << sizeof(std::int32_t) << endl;
+//    cout << sizeof(std::int64_t) << endl;
 //    cout << sizeof(float) << endl;
 //    cout << (171 / 16) << endl;
 //    cout << (171 >> 4) << endl;
@@ -24,34 +27,48 @@ int main()
     fix_point fp = to_fix_point(fl);
     fix_point fp2 = to_fix_point(9234.5678f);
     fix_point fp3 = to_fix_point(7);
-    fix_point fp4 = to_fix_point(-3);
+//    fix_point fp4 = to_fix_point(3);
+    fix_point fp4 = to_fix_point(-3.2);
 
-    cout << fl << endl;
-    cout << fp.data << endl;
-    cout << to_float(fp) << endl;
-    cout << floor(fp) << endl;
-    cout << frac(fp) << endl;
+    cout << "float        = " << fl << endl;
+    cout << "fp.data      = " << fp.data << endl;
+    cout << "to_float(fp) = " << to_float(fp) << endl;
+    cout << "floor(fp)    = " << floor(fp) << endl;
+    cout << "frac(fp)     = " << frac(fp) << endl;
 
-    cout << equals(fp, fp) << endl;
-    cout << equals(fp, fp2) << endl;
-    cout << equals(fp2, fp) << endl;
+    cout << "to_float(fp) = " << to_float(fp) << endl;
+    cout << "to_float(fp2) = " << to_float(fp2) << endl;
 
-    cout << less_than(fp, fp) << endl;
-    cout << less_than(fp, fp2) << endl;
-    cout << less_than(fp2, fp) << endl;
+//    cout << "equals(fp, fp)  = " << equals(fp, fp) << endl;
+//    cout << "equals(fp, fp2) = " << equals(fp, fp2) << endl;
+//    cout << "equals(fp2, fp) = " << equals(fp2, fp) << endl;
 
-    cout << to_float(add(fp3, fp4)) << endl;
-    cout << to_float(sub(fp3, fp4)) << endl;
-    cout << to_float(mul(fp3, fp4)) << endl;
-    cout << to_float(div(fp3, fp4)) << endl;
+//    cout << "less_than(fp, fp)  = " << less_than(fp, fp) << endl;
+//    cout << "less_than(fp, fp2) = " << less_than(fp, fp2) << endl;
+//    cout << "less_than(fp2, fp) = " << less_than(fp2, fp) << endl;
+
+    cout << "fp3.data = " << fp3.data << endl;
+    cout << "fp4.data = " << fp4.data << endl;
+    cout << "to_float(fp3) = " << to_float(fp3) << endl;
+    cout << "to_float(fp4) = " << to_float(fp4) << endl;
+
+    cout << "to_float(add(fp3, fp4)) = " << to_float(add(fp3, fp4)) << endl;
+    cout << "to_float(sub(fp3, fp4)) = " << to_float(sub(fp3, fp4)) << endl;
+    cout << "to_float(mul(fp3, fp4)) = " << to_float(mul(fp3, fp4)) << endl;
+    cout << "to_float(div(fp3, fp4)) = " << to_float(div(fp3, fp4)) << endl;
 
     const fix_point fpx = to_fix_point(-4.0f);
-    cout << floor(fpx) << endl;
+    cout << "floor(fpx) = " << floor(fpx) << endl;
 
 
-//    cout << ((std::int32_t) 0xffff0000) << endl;
+    const fix_point fp01 = to_fix_point(6.375f);
+    const fix_point fp02 = to_fix_point(-4.0f);
 
-//    cout << (((int) 0xff00) >> 8) << endl;
+    cout << to_float(div(fp01, fp02)) << endl;
+    cout << -1.59375f << endl;
+
+    cout << to_float(mul(fp01, fp02)) << endl;
+    cout << -25.5f << endl;
 
     return 0;
 }
