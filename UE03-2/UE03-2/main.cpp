2d72e32566794ab633311edde6e4bf3fbe0cce68
Sebastian Dassé <sebdas@web.de>
2015-01-10 21:56:26 +0100

    UE03-2 done.

--- UE03-2/UE03-2/main.cpp
+++ UE03-2/UE03-2/main.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <utility>
 #include <tuple>
+#include <future>
 #include "fix_point.hpp"
 
 using namespace std;
@@ -27,7 +28,7 @@ struct Slice {
 };
 
 
-void calc(fix_point<16, 16>& sum, Slice slice) {
+void calc_threads(fix_point<16, 16>& sum, Slice slice) {
     fix_point<16, 16> sliceSum = 0.f;
     for (auto it1 = slice.begin1, it2 = slice.begin2; it1 != slice.end1; it1++, it2++) {
         sliceSum += *it1 * *it2;
@@ -49,23 +50,56 @@ fix_point<16, 16> dot_product_threads(fix_point_vec& v1, fix_point_vec& v2) {
                     v1.begin() + hi,
                     v2.begin() + lo,
                     v2.begin() + hi);
-//        threads.emplace_back( calculate, std::ref(sum), slice );
-        threads[i] = std::thread(calc, std::ref(sum), slice);
+//        threads.emplace_back( calc_threads, std::ref(sum), slice );
+        threads[i] = std::thread(calc_threads, std::ref(sum), slice);
 //        cout << "thread #" << i << " created (" << lo << ", " << hi << ")" << endl;
     }
 
-    for (int i = 0; i < int(numThreads); i++) {
-        threads[i].join();
+    for (auto &thread: threads) {
+        thread.join();
 //        cout << "joined #" << i << endl;
     }
+//    for (int i = 0; i < int(numThreads); i++) {
+//        threads[i].join();
+//        cout << "joined #" << i << endl;
+//    }
     return sum;
 }
 
+fix_point<16, 16> calc_async(Slice slice) {
+    fix_point<16, 16> sliceSum = 0.f;
+    for (auto it1 = slice.begin1, it2 = slice.begin2; it1 != slice.end1; it1++, it2++) {
+        sliceSum += *it1 * *it2;
+    }
+    return sliceSum;
+}
+
 fix_point<16, 16> dot_product_async(fix_point_vec& v1, fix_point_vec& v2) {
+    const auto numThreads = std::thread::hardware_concurrency();
     fix_point<16, 16> sum(0.f);
     /*
      * TODO
      */
+
+//    std::vector< std::future< fix_point<16, 16> > > futures;
+    std::future< fix_point<16, 16> > futures[numThreads];
+    for (int i = 0; i < int(numThreads); i++) {
+        int lo = i * v1.size() / numThreads;
+        int hi = (i+1) * v1.size() / numThreads;
+        Slice slice(v1.begin() + lo,
+                    v1.begin() + hi,
+                    v2.begin() + lo,
+                    v2.begin() + hi);
+        futures[i] = std::async(std::launch::async, calc_async, slice);
+//        futures.push_back(std::async(std::launch::async, calc_async, slice));
+//        futures.emplace_back(std::launch::async, calc_async, slice );
+//        cout << "thread #" << i << " created (" << lo << ", " << hi << ")" << endl;
+    }
+
+    for (auto &future : futures) {
+        sum += future.get();
+    }
+
     return sum;
 }
 
@@ -91,11 +125,13 @@ int main() {
     const auto vec1 = std::vector< fix_point<16, 16> >(NUM_ELEMENTS, 0.5f);
     const auto vec2 = std::vector< fix_point<16, 16> >(NUM_ELEMENTS, 0.25f);
 
+    cout << "-- threads calculation --" << endl;
     auto result1 = executeClocked(dot_product_threads, vec1, vec2);
     cout << "result = " << result1 << endl;
 
     cout << endl;
 
+    cout << "-- async calculation --" << endl;
     auto result2 = executeClocked(dot_product_async, vec1, vec2);
     cout << "result = " << result2 << endl;
 
